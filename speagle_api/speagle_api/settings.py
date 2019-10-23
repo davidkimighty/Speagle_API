f85f5772e7c6446d1e003f8dbb167793e73ec748
HyunnnnKim <hwkim931224@gmail.com>
2019-10-23 13:47:52 +0900

    change of db
    into postgres

--- speagle_api/speagle_api/settings.py
+++ speagle_api/speagle_api/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'speagle_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'speagle',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
