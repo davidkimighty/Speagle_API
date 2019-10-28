ead802bc58fa880fe58ea5fd1f73dcb61b96e41f
HyunnnnKim <hwkim931224@gmail.com>
2019-10-29 00:38:15 +0900

    official DRF tutorial
    env changed

--- speagle_api/speagle/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-#Speagle Serializer
-class SpeagleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
