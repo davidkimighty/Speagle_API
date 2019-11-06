1b41f915d6cdb06b84a83964821b85c27d7255ef
HyunnnnKim <hwkim931224@gmail.com>
2019-11-06 17:06:55 +0900

    Extending AbstractBaseUser
    verification needed

--- server/speagle/models.py
+++ server/speagle/models.py
@@ -1,24 +1,70 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
 from django.conf import settings
 
 
-class User(AbstractUser):
-    username = models.CharField(max_length=20, blank=True, null=True)
-    email = models.EmailField(_('email address'), unique=True)
-    # username and email is redifined, so that it overrides the existing username and email field
-    # to set the unique parameter as True on email and blank parameters as True with username.  
-    #date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+class UserManager(BaseUserManager):
+    #use_in_migrations = True
 
-    USERNAME_FIELD = 'email' # email field is used as a username for authentication.
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    def _create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError('email must be set')
+        if not username:
+            raise ValueError('username must be set')
+
+        user = self.model(email=self.normalize_email(email), username=username, **extra_fields
+            )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(_('username'), max_length=10, unique=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    last_login = models.DateTimeField(_('last login'), auto_now=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return "{}".format(self.email)
+        return self.email
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
     dob = models.DateField()
-    country = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='uploads', blank=True) # ImageField requires pillow python package.
+    country = models.CharField(_('country'),max_length=50)
+    photo = models.ImageField(upload_to='uploads', null=True, blank=True)
