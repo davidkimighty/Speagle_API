1d1abd7975e56ed5f5c5e071c24cfe9dbb115b9b
HyunnnnKim <hwkim931224@gmail.com>
2019-11-12 17:39:56 +0900

    User registration complete
    but, sending the key through email by smtp is needed.

--- server/speagle/urls.py
+++ server/speagle/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path, re_path
 
-from .views import SendEmailForValidation
+from .views import *
 
 
 app_name = 'speagle'
 
 urlpatterns = [
-    re_path(r'^validate_email/', SendEmailForValidation.as_view())
+    re_path(r'^validate_email/', SendEmailForValidation.as_view()),
+    re_path("^validate_key/$", ValidateKey.as_view()),
+    re_path("^register/$", Register.as_view())
 ]
--- server/speagle/views.py
+++ server/speagle/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 import random
 from .models import User, EmailKey
 from .tokens import account_activation_token
+from .serializers import CreateUserSerializer
  
 
 class SendEmailForValidation(APIView):
@@ -22,12 +23,14 @@ class SendEmailForValidation(APIView):
                     'detail' : 'This email is already in use.'
                 })
             else:
-                key = generate_key(email)
+                key = send_email(email)
                 if key:
                     old = EmailKey.objects.filter(email__iexact = email)
+                    
                     if old.exists():
                         old = old.first()
                         count = old.count
+                        
                         if count > 6:
                             return Response({
                                 'status' : False,
@@ -57,7 +60,7 @@ class SendEmailForValidation(APIView):
                 'detail' : 'Email is needed for post request.'
             })
     
-def generate_key(email):
+def send_email(email):
         if email:
             key = random.randint(999, 9999)
             print(key)
@@ -72,11 +75,13 @@ class ValidateKey(APIView):
         email = request.data.get('email', False)
         key_sent = request.data.get('key', False)
 
-        if email & key_sent:
+        if email and key_sent:
             old = EmailKey.objects.filter(email__iexact = email)
+            
             if old.exists():
                 old = old.first()
                 key = old.key
+                
                 if str(key_sent) == str(key):
                     old.validated = True
                     old.save()
@@ -98,4 +103,46 @@ class ValidateKey(APIView):
             return Response({
                 'status' : False,
                 'detail' : 'Both email and key is needed for validation.'
+            })
+
+
+class Register(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email', False)
+        password = request.data.get('password', False)
+
+        if email and password:
+            old = EmailKey.objects.filter(email__iexact = email)
+            
+            if old.exists():
+                old = old.first()
+                validated = old.validated
+                
+                if validated:
+                    temp_data = {
+                        'email' : email,
+                        'password' : password
+                    }
+                    serializer = CreateUserSerializer(data = temp_data)
+                    serializer.is_valid(raise_exception=True)
+                    user = serializer.save()
+                    old.delete()
+                    return Response({
+                        'status' : True,
+                        'detail' : 'Account created.'
+                    })
+                else:
+                    return Response({
+                        'status' : False,
+                        'detail' : 'key varification needed.'
+                    })
+            else:
+                return Response({
+                    'status' : False,
+                    'detail' : 'Verify your email first.'
+                })
+        else:
+            return Response({
+                'status' : False,
+                'detail' : 'Both email and password needs to be set.'
             })
